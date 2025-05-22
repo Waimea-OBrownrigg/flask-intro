7c42fc60cc51eb4ddd6a65c51167625cf2f5231b
Waimea-OBrownrigg <ocbrownrigg@waimea.school.nz>
2025-05-22 12:24:02 +1200

    Made new pages and a form to gather data.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from random import randint
 
 app = Flask(__name__)
@@ -16,3 +17,21 @@ def about():
 def random():
     rand_num = (randint(1,1000000))
     return render_template('pages/rng.jinja', number = rand_num)
+
+@app.get("/number/<int:num>")
+def analyse_number(num):
+    print(f"you entered: {num}")
+    return render_template('pages/number.jinja', number = num)
+
+@app.get("/form/")
+def form():
+    return render_template("pages/form.jinja")
+
+@app.get("/secret/")
+def secret():
+    return render_template("pages/secret.jinja")
+
+
+@app.errorhandler(404)
+def notfound(e):
+    return render_template("pages/404.jinja")
